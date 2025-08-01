501a213e384adc26e352de037b0e3566876cbcfd
ispastro <haileasaye51@gmail.com>
2025-08-01 20:13:12 +0300

    match Routes modified

--- src/routes/matchRoutes.js
+++ src/routes/matchRoutes.js
@@ -4,7 +4,9 @@ import express from 'express';
 
 
 import {findSkillsMatches} from '../controllers/matchController.js';
-import {notifyNewMatches} from '../controllers/notifyController.js';
+import { notifyNewMatches } from '../controllers/notifyController.js';
+
+
 
 import authMiddleware from '../middleware/authMiddleware.js';
 
