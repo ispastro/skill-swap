c399e44009c40eae5edfa84c9a94aeb6c981ff28
ispastro <haileasaye51@gmail.com>
2025-08-13 09:52:36 +0300

    passport js is configured

---
+++ src/config/passport.js
@@ -0,0 +1,25 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+},
+async (accessToken, refreshToken, profile, done) => {
+  let user = await prisma.user.findUnique({ where: { email: profile.emails[0].value } });
+  if (!user) {
+    user = await prisma.user.create({
+      data: {
+        username: profile.displayName,
+        email: profile.emails[0].value,
+        password: '', // Google users don't need a password
+      }
+    });
+  }
+  return done(null, user);
+}));
+
+export default passport;
