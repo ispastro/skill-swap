089794e1bed38110d361b0861d3c49f62503f33e
Beza-12 <bezawithaile94@gmail.com>
2025-08-19 09:58:06 +0300

    request validator and result validator middleware applied

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import {login , register} from '../controllers/authController.js';
+import { registerValidator, loginValidator } from '../validators/authValidators.js';
+import { validateResult } from '../middleware/validateRequest.js';
 
 
 const router =express.Router();
 
-router.post('/login', login);
-router.post('/register', register);
+router.post('/login', loginValidator, validateResult, login);
+router.post('/register', registerValidator, validateResult, register);
 // Assuming you want to add this route
 
 export default router;
